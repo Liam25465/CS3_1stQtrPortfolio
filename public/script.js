bee85c51307c7fee1dc4d822b15292d486e83381
Liam25465 <b2029ljpsamiento@pshs.edu.ph>
2025-08-05 08:51:09 +0800

    non graded v3

--- public/script.js
+++ public/script.js
@@ -2,4 +2,4 @@
  var maxAge = 100;
  var numPerDay = 10;
  totalRequired = (numPerDay*365)*(maxAge-age);
- document.write("You will need " totalRequired " to last you until the ripe old age of " maxAge);
+ document.write("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
