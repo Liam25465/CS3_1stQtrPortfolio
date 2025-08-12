a5be37ac682792ff89c305288cf7c2191b73ded7
Liam25465 <b2029ljpsamiento@pshs.edu.ph>
2025-08-12 08:24:04 +0800

    nongraded edit 2

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
  let age = prompt("Enter your current age ", "1")
  let maxAge = prompt("Enter the max age you think you can live until ", "1")
  let numPerDay = prompt("Enter how many times you use/consume this item per day ", "1")
- confirm(`Is${age} really your age?`)
+ confirm(`Is ${age} really your age?`)
  if(age >= 1 && maxAge >= 1 && numPerDay >= 1) {
     totalRequired = (numPerDay*365)*(maxAge-age);
     document.write("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
