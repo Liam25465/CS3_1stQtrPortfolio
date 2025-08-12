0da5eb720f7d1f706aa69d166e289425ff582a03
Liam25465 <b2029ljpsamiento@pshs.edu.ph>
2025-08-12 08:42:49 +0800

    html nongraded edit

--- public/script.js
+++ public/script.js
@@ -1,11 +1,13 @@
- let age = prompt("Enter your current age ", "1")
- let maxAge = prompt("Enter the max age you think you can live until ", "1")
- let numPerDay = prompt("Enter how many times you use/consume this item per day ", "1")
- if(age >= 1 && maxAge >= 1 && numPerDay >= 1) {
-    totalRequired = (numPerDay*365)*(maxAge-age);
-    document.write("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
- }
- else {
-    alert("Any of the values cannot be less than 1, run the code again")
-    console.log("Value error")
- }
+ //let age = prompt("Enter your current age ", "1")
+ //let maxAge = prompt("Enter the max age you think you can live until ", "1")
+ //let numPerDay = prompt("Enter how many times you use/consume this item per day ", "1")
+ 
+ 
+ //if(age >= 1 && maxAge >= 1 && numPerDay >= 1) {
+    //totalRequired = (numPerDay*365)*(maxAge-age);
+    //document.write("You will need " + totalRequired + " to last you until the ripe old age of " + maxAge);
+ 
+ //else {
+    //alert("Any of the values cannot be less than 1, run the code again")
+    //console.log("Value error")
+ 
